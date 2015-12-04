2953d061e339bf79e996597e534eb0bc08cb1850
DileepNimantha <dileepnimantha@gmail.com>
2015-12-04 18:28:34 +0530

    Demo 'Autowired' and 'Qualifier' annotations

--- src/com/dileep/MainApp.java
+++ src/com/dileep/MainApp.java
@@ -4,13 +4,34 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
-	public static void main(String[] args) {
-		@SuppressWarnings("resource")
-		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	static ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
+	public static void main(String[] args) {
+		demoRequiredAnnotation();
+		demoAutowiredAnnotation();
+		demoQualifierAnnotation();
+	}
+
+	public static void demoRequiredAnnotation() {
+		System.out.println("\nDemo Required annotation");
 		Student student1 = (Student) context.getBean("student");
 
 		System.out.println("Name : " + student1.getName());
 		System.out.println("Age : " + student1.getAge());
+	}
+
+	public static void demoAutowiredAnnotation() {
+		System.out.println("\nDemo Autowired annotation");
+		TextEditor te = (TextEditor) context.getBean("textEditor");
+
+		te.spellCheck();
+	}
+
+	private static void demoQualifierAnnotation() {
+		System.out.println("\nDemo Qualifier annotation");
+		Profile profile = (Profile) context.getBean("profile");
+
+		profile.printAge();
+		profile.printName();
 	}
 }
